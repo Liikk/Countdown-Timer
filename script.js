5968e258441c7b90c2ffa840eb1e09e6d26047b5
Laguna <amar.lagumdzija.7@gmail.com>
2021-10-19 11:37:13 +0200

    Minor changes

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const newYears = "1 Jan 2022";
 function countdown(){
     const currentDate = new Date();
     const newYearsDate = new Date(newYears);
-    const TotalSeconds=(newYearsDate-currentDate)/1000;
+    const TotalSeconds = (newYearsDate-currentDate)/1000;
     const days = Math.floor(TotalSeconds/3600/24);
     const hours = Math.floor(TotalSeconds/3600) % 24;
     const minutes = Math.floor(TotalSeconds/60) % 60;
